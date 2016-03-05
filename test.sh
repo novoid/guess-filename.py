82a95c0498cccb51df10a31cd38ddd59b23093e6
Karl Voit <git@Karl-Voit.at>
2016-03-05 13:25:41 +0100

    added a testrun shell script which runs unit tests beforehand

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+myexit() {
+    echo "./guessfilename_test.sh returned ERROR(S). Please do test and try again."
+    exit 1
+}
+
+## run unit tests beforehand but dump output:
+./guessfilename_test.sh >/dev/null 2>&1 || myexit()
+
+echo
+
+./guessfilename.py -d testdata/2016-02-24\ A1\ Rechnung\ 02-2016\ -\ 12,12\ EUR\ --\ scan\ finance.pdf
+
+##end
